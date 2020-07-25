cbfdbcd849c6d84cb204e45ef900a5ccc4e7b3ac
Patrik Vodila <patrik.vodila@gmail.cz>
2020-07-25 04:28:19 +0200

    fix: hack for tmi.js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// hack for tmi.js
+(window as any).global = window;
